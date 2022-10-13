10037c28b373c4fe806e1c2a1343e5056c66c11c
AP Java Period 6 <apjavaperiod6@ac1mwk17bjc9h.78b505.nycboe.org>
2022-10-13 12:37:35 -0400

    Finished subString

--- 05-rng/RNGame.java
+++ 05-rng/RNGame.java
@@ -10,28 +10,87 @@ import java.util.Random;
 public class RNGame
 {
     public static void main(String[] args) {
+
+
+
+
         Random randNum = new Random();
-        int compNum = randNum.nextInt(3);
-        
-        System.out.println(" Lets try to guess the same number out of 3!");
+        System.out.print("Lets play a game. \nWhat kind of mode? \nEasy \nMedium \nHard\n");
+
+        Scanner mode = new Scanner(System.in);
+        String gameMode = mode.nextLine();
+        // mode.close();`
+
         Scanner input = new Scanner(System.in);
-        int Guess = input.nextInt();
-        
-        input.close();
-        
-        int change = Math.abs(compNum - Guess);
-        
-        
-        System.out.println(" Your guess was: " + Guess);
-        System.out.println(" I was thinking of: " + compNum);
-        
-        if (compNum == Guess){
-            System.out.println(" OMG you matched my number out of 3");
-        } else {
-            System.out.println(" Damn we were " + change + "apart, let's try again");
+        if (gameMode.equals("Easy")) {
+            int compNu = (int)(Math.random() * 3 + 1);
+            System.out.println(" Lets try to guess the same number out of 3!");
+            
+            int Guess = input.nextInt();
+            
+            input.close();
+            
+            int change = Math.abs(compNu - Guess);
+            
+            
+            System.out.println(" Your guess was: " + Guess);
+            System.out.println(" I was thinking of: " + compNu);
+            
+            if (compNu == Guess){
+                System.out.println(" OMG you matched my number out of 3");
+            } else {
+                System.out.println(" Damn we were " + change + " apart, let's try again");
+            }
+
+        } else if (gameMode.equals("Medium")) {
+            
+            int compNu =(int)(Math.random() * 30 + 1);;
+            System.out.println(" Lets try to guess the same number out of 30!");
+            
+            int Guess = input.nextInt();
+            
+            input.close();
+            
+            int change = Math.abs(compNu - Guess);
+            
+            
+            System.out.println(" Your guess was: " + Guess);
+            System.out.println(" I was thinking of: " + compNu);
+            
+            if (compNu == Guess){
+                System.out.println(" OMG you matched my number out of 30");
+            } else {
+                System.out.println(" Damn we were " + change + " apart, let's try again");
+            }
+
+        } else if (gameMode.equals("Hard")) {
+            
+            int compNu =(int)(Math.random() * 100 + 1);;
+            System.out.println(" Lets try to guess the same number out of 100!");
+            
+            int Guess = input.nextInt();
+            
+            input.close();
+            
+            int change = Math.abs(compNu - Guess);
+            
+            
+            System.out.println(" Your guess was: " + Guess);
+            System.out.println(" I was thinking of: " + compNu);
+            
+            if (compNu == Guess){
+                System.out.println(" OMG you matched my number out of 100");
+            } else {
+                System.out.println(" Damn we were " + change + " apart, let's try again");
+            }
+
+        }else {
+            System.out.println("Put in the modes either \nEasy \nMedium \nHard");
         }
         
+
         
+        mode.close();
         
         
         
---
+++ subString.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+
+
+public class subString {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Are you a Teacher or a Student?");
+        String role = s.nextLine(); 
+
+        System.out.println("Enter your first name:");
+        String firstName = s.nextLine();
+
+        System.out.println("Enter your last name:");
+        String lastName = s.nextLine();
+
+        System.out.println("Enter your favorite number:");
+        String favNumber = s.nextLine();
+
+       if (role.equals("Teacher")) {
+        role = "@schools.nyc.gov";
+        System.out.println("Your email is: ");
+        System.out.println(initialize(firstName) + lastName + favNumber + role);
+       } else if (role.equals("Student")) {
+        role = "@nycstudents.net";
+        System.out.println("Your email is: ");
+        System.out.println(firstName + initialize(lastName) + favNumber + role);
+
+       } else {
+        System.out.println("If you are a teacher, type Teacher. If you are a student, type Student");
+       }
+
+
+
+
+
+
+
+        s.close();
+
+
+
+
+
+}
+
+
+
+public static String initialize(String n) {
+    return n.substring(0, 1);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
